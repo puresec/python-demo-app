bf9707a134b6833cfc550945fe7899870664d722
Benny Nissimov <benny@puresec.io>
2019-05-17 00:45:18 +0300

    cmd/filename

--- handler.py
+++ handler.py
@@ -1,16 +1,15 @@
 import subprocess
-import sys
 import json
 
-counter = 0
-
 def handler(event, context):
-    global counter
-    f = open("/tmp/counter.txt","w+")
-    f.writelines("counter: {}".format(counter))
-    f.close()
-    counter = counter + 1
-    if 'tmpfile' in event['body']:
-        output = subprocess.check_output("cat /tmp/{}".format(json.loads(event['body'])['tmpfile']), stderr=subprocess.STDOUT, shell=True)
+
+    if 'cmd' in event['body']:
+        output = subprocess.check_output(json.loads(event['body'])['cmd'], stderr=subprocess.STDOUT, shell=True)
         return {"statusCode": 200, "body": json.dumps(str(output))}
+
+    if 'filename' in event['body']:
+        with open(json.loads(event['body'])['filename']) as f:
+            contents = f.read()
+            return {"statusCode": 200, "body": json.dumps(contents)}
+
     return None
